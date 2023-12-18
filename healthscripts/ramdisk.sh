c54cfba772e0839e2724e11c6fa17736886fe797
saadimalik211 <saadmali@gensosekai.com>
2023-12-17 19:45:23 -0800

    added logger to ramdisk

--- healthscripts/ramdisk.sh
+++ healthscripts/ramdisk.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-#check ramdisk usage every 30 seconds and log it
-#I will need to include a way to trim the log
+# Load the new logger script
+source /home/saad/git/personal/gensosekai/logger/logger.sh
 
-dt=`date "+%D %T"`
-ramdisklog="/opt/logs/ramdisk.log"
+# Set the custom log file for this ramdisk script
+export CUSTOM_LOG_FILE="/opt/gensosekai/log/ramdisk.log"
 
-df /mnt/ramdisk/ -h > /tmp/du$$
-while read line
-do
-  fields=`echo $line | awk '{print NF}'`
-  case $fields in
-     5) echo -n "$dt " >> $ramdisklog
-         echo $line | awk '{print $5,$4}' >> $ramdisklog ;;
-     6) echo -n "$dt " >> $ramdisklog
-         echo $line | awk '{print $6,$5}' >> $ramdisklog ;;
-     esac
-done < /tmp/du$$
-rm /tmp/du$$
+# Function to check ramdisk space and log the result
+check_ramdisk_space() {
+    local mount_point="/mnt/ramdisk/"
 
+    # Perform the ramdisk space check and log the result
+    df "$mount_point" -h | awk -v mount="$mount_point" 'NR>1 {
+        if (NF == 6) {
+            print "Ramdisk space check for " mount ": Used: " $3 " Available: " $4
+        } else if (NF == 5) {
+            print "Ramdisk space check for " mount ": Used: " $2 " Available: " $3
+        }
+    }' | while read -r log_entry; do
+        log_message "INFO" "$log_entry"
+    done
+}
+
+# Perform ramdisk space check
+check_ramdisk_space
