954249c9eedc986d19fd27019ca42c09ef6fa435
saadimalik211 <saadmali@gensosekai.com>
2023-12-17 19:27:35 -0800

    added logger to disktracker

--- healthscripts/DiskTracker.sh
+++ healthscripts/DiskTracker.sh
@@ -1,53 +1,32 @@
 #!/bin/bash
 
-dt=`date +%D`
-raid5log="/opt/logs/raid5space.log"
-backuplog="/opt/logs/backupspace.log"
-hostdisklog="/opt/logs/hostdisk.log"
+# Load the new logger script
+source /home/saad/git/personal/gensosekai/logger/logger.sh
 
-#raid5check
-df /mnt/raid5/ -h > /tmp/du$$
-while read line
-do
-    fields=`echo $line | awk '{print NF}'`
-    case $fields in
-    5) echo -n "$dt " >> $raid5log
-        echo $line | awk '{print $5,$4}' >> $raid5log ;;
-    6) echo -n "$dt " >> $raid5log
-        echo $line | awk '{print $6,$5}' >> $raid5log ;;
-    esac
-done < /tmp/du$$
-rm /tmp/du$$
-#end raid5check
+# Define the single log file path
+export CUSTOM_LOG_FILE="/opt/gensosekai/log/disktracker.log"
 
+# Function to check disk space and log the result
+check_disk_space() {
+    local mount_point=$1
+    local dt=$(date '+%Y-%m-%d %H:%M:%S')
 
-#backupcheck
-df /mnt/backup/ -h >> /tmp/du$$
-while read line
-do
-    fields=`echo $line | awk '{print NF}'`
-    case $fields in
-    5) echo -n "$dt " >> $backuolog
-        echo $line | awk '{print $5,$4}' >> $backuplog ;;
-    6) echo -n "$dt " >> $backuplog
-        echo $line | awk '{print $6,$5}' >> $backuplog ;;
-    esac
-done < /tmp/du$$
-rm /tmp/du$$
-#end backupcheck
+    # Perform the disk space check and log the result
+    df "$mount_point" -h | awk -v mount="$mount_point" 'NR>1 {
+        if (NF == 6) {
+            print "Disk space check for " mount ": Used: " $3 " Available: " $4
+        } else if (NF == 5) {
+            print "Disk space check for " mount ": Used: " $2 " Available: " $3
+        }
+    }' | while read -r log_entry; do
+        log_message "INFO" "$log_entry"
+    done
+}
 
+# Perform disk space checks for each mount point
+check_disk_space "/mnt/raid5/"
+check_disk_space "/mnt/backup/"
+check_disk_space "/dev/sda6"
 
-#hostdrivecheck
-df /dev/sda6 -h >> /tmp/du$$
-while read line
-do
-    fields=`echo $line | awk '{print NF}'`
-    case $fields in
-    5) echo -n "$dt " >> $hostdisklog
-        echo $line | awk '{print $5,$4}' >> $hostdisklog ;;
-    6) echo -n "$dt " >> $hostdisklog
-        echo $line | awk '{print $6,$5}' >> $hostdisklog ;;
-    esac
-done < /tmp/du$$
-rm /tmp/du$$
-#end hostdrivecheck
+# Log completion
+log_message "INFO" "Disk space checks completed."
