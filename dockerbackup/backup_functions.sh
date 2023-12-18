14e7844b98e8ceaf370c572fed2eda618390690b
saadimalik211 <saadmali@gensosekai.com>
2023-12-17 19:24:13 -0800

    added logger to docker backup

--- dockerbackup/backup_functions.sh
+++ dockerbackup/backup_functions.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-# Define the log file
-LOG_FILE="/opt/logs/backupdocker.log"
-
-# Function to log a message with timestamp
-log_message() {
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> "$LOG_FILE"
-}
+# Source the new logger script
+source /home/saad/git/personal/gensosekai/logger/logger.sh
 
 # Function to perform rsync
 do_rsync() {
-  log_message "Starting backup of volume: $1"
-  rsync -avh --delete "${SOURCE_DIR}/$1/" "${BACKUP_DIR}/$1/" || exit 1
-  log_message "Backup of volume $1 completed."
+  log_message "INFO" "Starting backup of volume: $1"
+  rsync -avh --delete "${SOURCE_DIR}/$1/" "${BACKUP_DIR}/$1/" || {
+    log_message "ERROR" "Rsync failed for volume: $1"
+    exit 1
+  }
+  log_message "INFO" "Backup of volume $1 completed."
 }
 
 # Function to stop and start containers
 manage_containers() {
   local action=$1
   shift
-  log_message "${action^} containers: $@"
+  log_message "INFO" "${action^} containers: $@"
   for container in "$@"; do
-    log_message "${action^} container: $container"
-    docker "$action" "$container" || exit 1
+    log_message "INFO" "${action^} container: $container"
+    docker "$action" "$container" || {
+      log_message "ERROR" "Docker failed to ${action} container: $container"
+      exit 1
+    }
   done
-  log_message "Containers ${action^}ed."
+  log_message "INFO" "Containers ${action^}ed."
 }
--- dockerbackup/backupdocker.sh
+++ dockerbackup/backupdocker.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Load the new logger script
+source /home/saad/git/personal/gensosekai/logger/logger.sh
+
+# Set and export the custom log file for this backup script
+export CUSTOM_LOG_FILE="/opt/gensosekai/log/backupdocker.log"
+
 # Load configuration from the YAML file
 CONFIG_FILE="/home/saad/git/personal/gensosekai/dockerbackup/backup-config.yml"
 BACKUP_DIR=$(yq e '.backup_directory' "$CONFIG_FILE")
@@ -9,7 +15,7 @@ SOURCE_DIR=$(yq e '.source_directory' "$CONFIG_FILE")
 source /home/saad/git/personal/gensosekai/dockerbackup/backup_functions.sh
 
 # Log the start time of the entire script
-log_message "Backup script started."
+log_message "INFO" "Backup script started."
 
 # Read the containers from the YAML file and backup each container's volumes
 yq e '.containers[]' "$CONFIG_FILE" -o json | jq -c '.' | while read -r container; do
@@ -30,4 +36,4 @@ yq e '.containers[]' "$CONFIG_FILE" -o json | jq -c '.' | while read -r containe
 done
 
 # Log the end time of the entire script
-log_message "Backup script completed."
+log_message "INFO" "Backup script completed."
